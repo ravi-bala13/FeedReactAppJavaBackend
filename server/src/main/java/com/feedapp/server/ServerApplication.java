140897fc3d07e25d54da1ce69ec45f5c9c33ce65
Bala Murugan <bals161616@gmail.com>
2023-06-18 06:48:56 +0530

    connected with mongodb

---
+++ server/src/main/java/com/feedapp/server/ServerApplication.java
@@ -0,0 +1,34 @@
+package com.feedapp.server;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Log4j2
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(final ApplicationContext ctx) {
+		return args -> {
+			log.info("=====================  Application started =====================");
+		};
+	}
+
+	@GetMapping("/")
+	@ResponseStatus(HttpStatus.OK)
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
