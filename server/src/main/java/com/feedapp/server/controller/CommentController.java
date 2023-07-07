d67b479c5d1492959b1b22fde709f128c57b33a5
Bala Murugan <bals161616@gmail.com>
2023-07-07 21:56:58 +0530

    get all comments api added

--- server/src/main/java/com/feedapp/server/controller/CommentController.java
+++ server/src/main/java/com/feedapp/server/controller/CommentController.java
@@ -2,17 +2,19 @@ package com.feedapp.server.controller;
 
 import com.feedapp.server.request.PostDetailsRequest;
 import com.feedapp.server.response.BaseResponse;
+import com.feedapp.server.service.CommentService;
 import com.feedapp.server.utils.BaseResponseUtils;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @Log4j2
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
+
+    @Autowired
+    CommentService commentService;
 
     /**
      * This api is used to create a comment for a post by a user
@@ -21,8 +23,23 @@ public class CommentController {
     public BaseResponse<Object> createComment(@RequestBody PostDetailsRequest postDetailsRequest){
         try {
             log.info("Received Request in createPost -{}", postDetailsRequest);
-
+            commentService.saveComment(postDetailsRequest);
             return BaseResponseUtils.createSuccessBaseResponse();
+        }catch (Exception e){
+            log.error("Error in createPost", e);
+            return BaseResponseUtils.createErrorBaseResponse();
+        }
+    }
+
+    /**
+     * This api is used to fetch all comments for a particular post in paginated manner which are recently commented
+     */
+    @GetMapping("/")
+    public BaseResponse<Object> getAllPosts(@RequestParam(value = "offset", required = false) Integer offset, @RequestParam(value = "limit", required = false) Integer limit){
+        try {
+            offset = (offset == null || offset <= 0) ? 0 : offset-1;
+            limit = (limit == null || limit > 5) ? 5 : limit;
+            return commentService.getAllPost(offset, limit);
         }catch (Exception e){
             log.error("Error in createPost", e);
             return BaseResponseUtils.createErrorBaseResponse();
--- server/src/main/java/com/feedapp/server/service/CommentService.java
+++ server/src/main/java/com/feedapp/server/service/CommentService.java
@@ -2,14 +2,19 @@ package com.feedapp.server.service;
 
 import com.feedapp.server.database.mongo.Dao.CommentDao;
 import com.feedapp.server.database.mongo.Entity.Comment;
-import com.feedapp.server.database.mongo.Entity.Post;
 import com.feedapp.server.request.PostDetailsRequest;
+import com.feedapp.server.response.BaseResponse;
+import com.feedapp.server.utils.BaseResponseUtils;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 @Log4j2
 @Service
@@ -30,4 +35,16 @@ public class CommentService {
         log.info("saving post - {}", result.getId());
     }
 
+    public BaseResponse<Object> getAllPost(Integer offset, Integer limit) {
+        List<Comment> commentList;
+        try {
+            Pageable pageable = PageRequest.of(offset, limit);
+            Page<Comment> postPageable = commentDao.findAll(pageable);
+            commentList = postPageable.toList();
+            return BaseResponseUtils.createSuccessBaseResponse(commentList);
+        }catch (Exception e){
+            log.error("Exception in getAllPost", e);
+            return BaseResponseUtils.createErrorBaseResponse();
+        }
+    }
 }
